c415f14ec0cc8626ebe9b17365a252cc51022ec9
Yun Ju‚ Tseng <ricerice@seasonjoy-lm.tpcity.corp.yahoo.com>
2015-10-07 16:03:54 +0800

    pemory add

---
+++ framer/views.ui_test.js
@@ -0,0 +1,391 @@
+		window.FramerPS = window.FramerPS || {};
+		window.FramerPS['ui_test'] = [
+	{
+		"id": 1292,
+		"name": "content",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1134
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/content.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 640,
+				"height": 723
+			}
+		},
+		"imageType": "png",
+		"children": [
+			{
+				"id": 1815,
+				"name": "content",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1134
+				},
+				"maskFrame": null,
+				"image": null,
+				"imageType": null,
+				"children": [
+					{
+						"id": 1737,
+						"name": "Picture",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Picture.png",
+							"frame": {
+								"x": 59,
+								"y": 355,
+								"width": 128,
+								"height": 128
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "369795302"
+					},
+					{
+						"id": 1728,
+						"name": "Folder",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Folder.png",
+							"frame": {
+								"x": 257,
+								"y": 355,
+								"width": 128,
+								"height": 128
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "549958643"
+					},
+					{
+						"id": 1722,
+						"name": "Document",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Document.png",
+							"frame": {
+								"x": 455,
+								"y": 355,
+								"width": 128,
+								"height": 128
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "898986942"
+					},
+					{
+						"id": 1813,
+						"name": "Compass",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Compass.png",
+							"frame": {
+								"x": 59,
+								"y": 525,
+								"width": 128,
+								"height": 128
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "1695270446"
+					},
+					{
+						"id": 1801,
+						"name": "Dashboard",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Dashboard.png",
+							"frame": {
+								"x": 257,
+								"y": 525,
+								"width": 128,
+								"height": 128
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "415130467"
+					},
+					{
+						"id": 1794,
+						"name": "Chart",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Chart.png",
+							"frame": {
+								"x": 455,
+								"y": 525,
+								"width": 128,
+								"height": 128
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "109140939"
+					}
+				],
+				"modification": "1852243627"
+			},
+			{
+				"id": 1441,
+				"name": "oscars&r badge",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1134
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/oscars&r badge.png",
+					"frame": {
+						"x": 168,
+						"y": 154,
+						"width": 295,
+						"height": 49
+					}
+				},
+				"imageType": "png",
+				"children": [
+					{
+						"id": 1444,
+						"name": "Group 5",
+						"layerFrame": {
+							"x": 0,
+							"y": 0,
+							"width": 640,
+							"height": 1134
+						},
+						"maskFrame": null,
+						"image": {
+							"path": "images/Group 5.png",
+							"frame": {
+								"x": 196,
+								"y": 162,
+								"width": 238,
+								"height": 22
+							}
+						},
+						"imageType": "png",
+						"children": [
+							
+						],
+						"modification": "1892339548"
+					}
+				],
+				"modification": "1597906427"
+			},
+			{
+				"id": 1244,
+				"name": "tab",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1134
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/tab.png",
+					"frame": {
+						"x": 0,
+						"y": 220,
+						"width": 640,
+						"height": 639
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "631354261"
+			}
+		],
+		"modification": "1594536463"
+	},
+	{
+		"id": 1437,
+		"name": "Navigation Bar",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1134
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Navigation Bar.png",
+			"frame": {
+				"x": 0,
+				"y": 39,
+				"width": 640,
+				"height": 94
+			}
+		},
+		"imageType": "png",
+		"children": [
+			{
+				"id": 1780,
+				"name": "Group 7 copy 3",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1134
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/Group 7 copy 3.png",
+					"frame": {
+						"x": 570,
+						"y": 67,
+						"width": 40,
+						"height": 38
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "1016614705"
+			},
+			{
+				"id": 1436,
+				"name": "expand menu",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1134
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/expand menu.png",
+					"frame": {
+						"x": 38,
+						"y": 74,
+						"width": 24,
+						"height": 19
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "111967801"
+			},
+			{
+				"id": 1785,
+				"name": "Group 7 copy 4",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1134
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/Group 7 copy 4.png",
+					"frame": {
+						"x": 30,
+						"y": 65,
+						"width": 40,
+						"height": 38
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "1386644033"
+			}
+		],
+		"modification": "747734198"
+	},
+	{
+		"id": 188,
+		"name": "Status Bar",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1134
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Status Bar.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 640,
+				"height": 40
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "650359489"
+	}
+]
